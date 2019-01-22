6db1a3b84b480fe1f3e25012ea67db344bde5e2b
zhangshuai <zhangshuai@ruizhihuida.com>
2019-01-22 15:10:43 +0800

    修改问题

--- js/addr.js
+++ js/addr.js
@@ -1,3 +1,8 @@
+//内蒙古
 mui.plusReady(function(){
-	localStorage.setItem('url','http://192.168.191.1:8081/Funds');
+	localStorage.setItem('url','http://121.57.156.30:30000/Funds');
 })
+//本地
+/* mui.plusReady(function(){
+	localStorage.setItem('url','http://192.168.1.104:8081/Funds');
+}) */
