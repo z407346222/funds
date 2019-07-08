c19640499b3a8d6ae20dff6d5bb312120c1bc8aa
zhangshuai <zhangshuai@ruizihhuida.com>
2019-07-08 13:55:46 +0800

    修改需求问题

--- js/addr.js
+++ js/addr.js
@@ -3,6 +3,6 @@ mui.plusReady(function(){
 	localStorage.setItem('url','http://121.57.156.30:30000/Funds');
 })
 //本地
-/* mui.plusReady(function(){
-	localStorage.setItem('url','http://192.168.1.104:8081/Funds');
-}) */
+// mui.plusReady(function(){
+// 	localStorage.setItem('url','http://192.168.3.11:8081/Funds');
+// })
